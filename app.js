26fcbd9e92d57003f18c7031d2abadcf169e59e4
Cat <paisley@hotmail.com>
2020-01-13 16:56:36 -0500

    Serving static HTML files (finally!)

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 const
   http = require('http'),
   fs = require('fs'),
+  url = require('url'),
   { createStore } = require('redux'),
+  { parse } = require('node-html-parser'),
   { todoApp } = require('./src/reducers'),
   { once } = require('./src/helpers'),
   {
@@ -13,6 +15,7 @@ const
   } = require('./src/actions'),
   { render } = require('./src/render');
 
+/*
 // Defines the store by passing the root reducer to `createStore`
 const portNumber = 8000;
 const store = createStore(todoApp);
@@ -37,7 +40,62 @@ const unsubscribe = store.subscribe( (suppressLogs = suppress) => {
 
 // Defines the handler for requests from the browser
 const handleRequest = (request, response) => {
-  fs.readFile('./index.html', null, function(error, data){
+  var http = require('http');
+  var url = require('url');
+  var fs = require('fs');
+*/  
+  var server = http.createServer(function(request, response) {
+    var path = url.parse(request.url).pathname;
+    switch (path) {
+      case '/':
+        response.writeHead(200, {
+          'Content-Type': 'text/plain'
+        });
+        response.write("This is Test Message.");
+        response.end();
+        break;
+      case '/HtmlPage1.html':
+        fs.readFile(__dirname + path, function(error, data) {
+          if (error) {
+            console.log(error);
+            response.writeHead(404);
+            response.write(error.toString());
+            response.end();
+          }
+          else {
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+            console.log(data);
+            response.write(Buffer.from(data));
+            response.end();
+          }
+        });
+        break;
+      case '/HtmlPage2.html':
+        fs.readFile(__dirname + path, function(error, data) {
+          if (error) {
+            console.log(error);
+            response.writeHead(404);
+            response.write(error.toString());
+            response.end();
+          }
+          else {
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.write(Buffer.from(data));
+            response.end();
+          }
+        });
+        break;
+      default:
+        response.writeHead(404);
+        response.write("oops this doesn't exist - 404");
+        response.end();
+        break;
+    }
+  });
+  server.listen(8082);
+
+  /*
+  fs.readFile('./index.txt', function(error, data){
     if(error){
       console.log("404");
       response.writeHead(404);
@@ -45,10 +103,12 @@ const handleRequest = (request, response) => {
     }
     else{
       response.writeHead(200, { 'Content-Type': 'text/html' });
-      //console.log("data:"); console.log(data);
+      
+      console.log("data:"); console.log(data);
 
-
-      // parse HTML here (w/ `node-html-parser`), insert dynamic elements, and write the result
+      // parse HTML here? (w/ `node-html-parser`), insert dynamic elements, and write the result
+      //const parsed = parse(data);       
+      //console.log(parsed.firstChild.structure);
 
       // This succeeds (is meant to be integrated into the markup read from index.html below
       response.write(render(store.getState()));
@@ -65,18 +125,24 @@ const handleRequest = (request, response) => {
     }
     response.end();
   });
+  
+
 };
 
 // Sets up the server
 http.createServer(handleRequest).listen(portNumber);
+*/
+
 
 // Dispatches some test actions
+/*
 store.dispatch(addTodo('Learn about actions'));
 store.dispatch(addTodo('Learn about reducers'));
 store.dispatch(addTodo('Learn about store'));
 store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_ACTIVE));
 store.dispatch(toggleTodo(0));
 store.dispatch(toggleTodo(1));
+*/
 
 // Stops listening to state updates
-unsubscribe();
+//unsubscribe();
