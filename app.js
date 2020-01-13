5dc398565cc5bf68402277d6bdad7acd7c9f79db
Cat <paisley@hotmail.com>
2020-01-13 02:37:19 -0500

    Trying to use index.html as a shell (See comments re: errors)

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Imports redux.createStore, reducers.todoApp, and the props from `actions`
 const
   http = require('http'),
+  fs = require('fs'),
   { createStore } = require('redux'),
   { todoApp } = require('./src/reducers'),
   { once } = require('./src/helpers'),
@@ -27,23 +28,43 @@ console.log(store.getState());
 //   - Add it to stateHistory
 //   - Log it (unless suppress == true)
 // (Note that subscribe() returns a function for unregistering the listener)
-let suppress; //suppress = true;
+let suppress; suppress = true;
 const unsubscribe = store.subscribe( (suppressLogs = suppress) => {
   stateHistory.push(store.getState);
-  if(!suppressLogs){
-    console.log(store.getState());
-  }
+  if(!suppressLogs){ console.log(store.getState()); }
 });
 
 
 // Defines the handler for requests from the browser
 const handleRequest = (request, response) => {
-  response.writeHead(200, {
-    'Content-Type': 'text/html'
+  fs.readFile('./index.html', null, function(error, data){
+    if(error){
+      console.log("404");
+      response.writeHead(404);
+      response.write('404 :(');
+    }
+    else{
+      response.writeHead(200, { 'Content-Type': 'text/html' });
+      //console.log("data:"); console.log(data);
+
+
+      // parse HTML here (w/ `node-html-parser`), insert dynamic elements, and write the result
+
+      // This succeeds (is meant to be integrated into the markup read from index.html below
+      response.write(render(store.getState()));
+
+      // This succeeds
+      response.write('<!DOCTYPE html><div style="color:blue">woo</div>');
+
+      // This fails, not sure why (Happens even if index.html contains markup identical to the above string)
+      response.write(data);
+
+      // This writes: {"type":"Buffer","data":[60,33,68,79,67,84,89,80,69,32 ... ]}
+      response.write(JSON.stringify(data));
+
+    }
+    response.end();
   });
-  response.write(render(store.getState()));
-  //response.write(JSON.stringify(store.getState()));
-  response.end();
 };
 
 // Sets up the server
