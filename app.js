d219be9721482f63d8de3a187279ea3b4ae9fa25
Cat <paisley@hotmail.com>
2020-01-13 17:18:23 -0500

    Comment

--- app.js
+++ app.js
@@ -43,7 +43,8 @@ const handleRequest = (request, response) => {
   var http = require('http');
   var url = require('url');
   var fs = require('fs');
-*/  
+*/
+  // (From: https://www.c-sharpcorner.com/article/creating-server-and-host-html-page-using-node-js)
   var server = http.createServer(function(request, response) {
     var path = url.parse(request.url).pathname;
     switch (path) {
