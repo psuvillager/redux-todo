ebd2fed758d46b89aaee96138ee5818844e71925
Cat <paisley@hotmail.com>
2020-01-14 04:34:35 -0500

    Serving custom static content

--- app.js
+++ app.js
@@ -48,52 +48,27 @@ const handleRequest = (request, response) => {
   var server = http.createServer(function(request, response) {
     var path = url.parse(request.url).pathname;
     switch (path) {
-      case '/':
-        response.writeHead(200, {
-          'Content-Type': 'text/plain'
-        });
-        response.write("This is Test Message.");
-        response.end();
-        break;
+      case '/': path = '/index.html';
+      case '/index.html':
       case '/HtmlPage1.html':
         fs.readFile(__dirname + path, function(error, data) {
-          if (error) {
-            console.log(error);
-            response.writeHead(404);
-            response.write(error.toString());
-            response.end();
-          }
-          else {
-            response.writeHead(200, { 'Content-Type': 'text/html' });
-            console.log(data);
-            response.write(Buffer.from(data));
-            response.end();
-          }
+          if(error){ write404(error); }
+          else { response.writeHead(200, { 'Content-Type': 'text/html' }); response.end(Buffer.from(data)); }
         });
         break;
-      case '/HtmlPage2.html':
-        fs.readFile(__dirname + path, function(error, data) {
-          if (error) {
-            console.log(error);
-            response.writeHead(404);
-            response.write(error.toString());
-            response.end();
-          }
-          else {
-            response.writeHead(200, { 'Content-Type': 'text/html' });
-            response.write(Buffer.from(data));
-            response.end();
-          }
-        });
-        break;
-      default:
-        response.writeHead(404);
-        response.write("oops this doesn't exist - 404");
-        response.end();
-        break;
+      default: write404("unknown file: " + path);
     }
+
+    function write404(err){
+      console.log(err);
+      response.writeHead(404, { 'Content-Type': 'text/html' });
+      response.write(err.toString());
+      response.end();
+    }
+
   });
   server.listen(8082);
+
 
   /*
   fs.readFile('./index.txt', function(error, data){
