c6a57d9c53e01a723e4cd403a5604234d0768f29
Cat <paisley@hotmail.com>
2020-01-16 00:14:37 -0500

    Cleanup and comments

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-// Imports redux.createStore, reducers.todoApp, and the props from `actions`
 const
+  // Imports node modules
   http = require('http'),
   fs = require('fs'),
   url = require('url'),
   { createStore } = require('redux'),
   { parse } = require('node-html-parser'),
+
+  // Imports app resources
   { todoApp } = require('./src/reducers'),
   { once } = require('./src/helpers'),
   {
@@ -14,79 +16,76 @@ const
     VisibilityFilters
   } = require('./src/actions'),
   { render } = require('./src/render'),
-  PORT_NUMBER = 8082;
 
-/*
+  // Sets other constants
+  PORT_NUMBER = 8082
+;
+
 // Defines the store by passing the root reducer to `createStore`
-const portNumber = 8000;
 const store = createStore(todoApp);
 
 // Defines a global history array and pushes the initial state to it
-const stateHistory = [];
-stateHistory.push(store.getState());
+const stateHistory = []; stateHistory.push(store.getState());
 
-// Logs the initial state
-console.log(store.getState());
-
-// Every time the state changes
-//   - Add it to stateHistory
-//   - Log it (unless suppress == true)
-// (Note that subscribe() returns a function for unregistering the listener)
+// Allows suppressing logs
 let suppress; suppress = true;
+
+// Registers a listener for any changes to state (`.subscribe` returns a function for unregistering)
 const unsubscribe = store.subscribe( (suppressLogs = suppress) => {
   stateHistory.push(store.getState);
   if(!suppressLogs){ console.log(store.getState()); }
 });
 
-// Defines the handler for requests from the browser
-const handleRequest = (request, response) => {
-  var http = require('http');
-  var url = require('url');
-  var fs = require('fs');
-*/
-  // (From: https://www.c-sharpcorner.com/article/creating-server-and-host-html-page-using-node-js)
-  const server = http.createServer(handleRequest);
-  server.listen(PORT_NUMBER);
+// Defines the server for the browser to send requests to
+// (See www.c-sharpcorner.com/article/creating-server-and-host-html-page-using-node-js)
+const server = http.createServer(handleRequest);
+server.listen(PORT_NUMBER);
 
-  function handleRequest(request, response){
-    let path = url.parse(request.url).pathname;
-    switch(path){
-      case '/favicon.ico': break; // Does nothing
-      case '/': path = '/index.html'; // Uses main page
-      case '/index.html': // Continues to next case
-      case '/HtmlPage1.html': // Tries to read file at `path` and responds accordingly
-        fs.readFile(__dirname + path, function(error, data){
-          // Calls `serve404` if necessary
-          if(error){ serve404(request, response, path, error); }
-          // Otherwise, converts file object to a Buffer, and write it to response
-          else{ response.writeHead(200, { 'Content-Type': 'text/html' }); response.end(Buffer.from(data)); }
-        });
-        break;
-      default: serve404(request, response, path, "unknown file: " + path);
+// Defines how the server will respond to requests from the browser
+// Needs to trigger store updates and inform the browser of the new state (for client-side rendering)
+//   or built new HTML from the new state and inform it of the new HTML (server-side rendering)
+function handleRequest(request, response){
+  let path = url.parse(request.url).pathname;
+  switch(path){
+    case '/favicon.ico': break; // Does nothing
+    case '/': path = '/index.html'; // Uses main page
+    case '/index.html': // Continues to next case
+    case '/HtmlPage1.html': // Tries to read file at `path` and responds accordingly
+      fs.readFile(__dirname + path, function(error, data){
+        // Calls `serve404` if necessary
+        if(error){ serve404(request, response, path, error); }
+        // Otherwise, converts file object to a Buffer, and writes it to response
+        else{
+          response.writeHead(200, { 'Content-Type': 'text/html' });
+          response.write(Buffer.from(data)); }
+          response.end();
+      });
+      break;
+    default: serve404(request, response, path, "unknown file: " + path);
+  }
+}
+
+function serve404(req, res, path, err){
+  console.log(err);
+  fs.readFile(__dirname + '404', function(errorPageError, errorPageData){
+    if(errorPageError){
+      // In case the 404 page itself is not found
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end(errorPageError.toString());
     }
-  }
+    else{
+      res.writeHead(404, { 'Content-Type': 'text/html' });
+      res.end(Buffer.from(errorPageData)); }
+  });
+}
 
-  function serve404(req, res, path, err){
-    console.log(err);
-    fs.readFile(__dirname + '404', function(errorPageError, errorPageData){
-      if(errorPageError){
-        // In case the 404 page itself is not found
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end(errorPageError.toString());
-      }
-      else{
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        res.end(Buffer.from(errorPageData)); }
-    });
-  }
+/*
+// parse HTML w/ `node-html-parser`, insert dynamic elements, and write the result, something like:
+//const parsed = parse(data);       
+//console.log(parsed.firstChild.structure);
 
-  /*
-  // parse HTML w/ `node-html-parser`, insert dynamic elements, and write the result, something like:
-  //const parsed = parse(data);       
-  //console.log(parsed.firstChild.structure);
-
-  // This succeeds (is meant to be integrated into the markup read from index.html below
-  response.write(render(store.getState()));
+// This succeeds (is meant to be integrated into the markup read from index.html below
+response.write(render(store.getState()));
 
 // Sets up the server
 http.createServer(handleRequest).listen(portNumber);
